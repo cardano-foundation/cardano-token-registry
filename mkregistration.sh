ea22a7aa67c991d0aa0141ef22b72ce292cf4b9a
Per <per@cardy-b>
2021-07-30 11:12:08 +0200

    First registration

---
+++ mkregistration.sh
@@ -0,0 +1,28 @@
+WAX_PATH=~/vault/mainnet/wallets/waxcoin
+
+ID=$(cat $WAX_PATH/waxcoin-policy-id)
+NAME=$(echo -n "waxcoin" | xxd -ps)
+
+SUBJECT=$ID$NAME
+
+rm $SUBJECT*
+
+token-metadata-creator entry --init $SUBJECT
+
+token-metadata-creator entry $SUBJECT \
+  --name "WaxCoin" \
+  --description "WaxCoin - stay hard and stylish." \
+  --policy $WAX_PATH/policy.script
+
+
+token-metadata-creator entry $SUBJECT \
+  --ticker "WAXC" \
+  --logo "waxcoin-128.png" \
+  --decimals 0
+
+
+token-metadata-creator entry $SUBJECT \
+  -a $WAX_PATH/policy.skey
+
+
+token-metadata-creator entry $SUBJECT --finalize
